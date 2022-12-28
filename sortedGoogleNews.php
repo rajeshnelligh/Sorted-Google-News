d21eac78eedf1452876dc2b295dc1bcc536036f7
rajeshnelligh <55105955+rajeshnelligh@users.noreply.github.com>
2022-12-28 19:51:55 +0530

    Update sortedGoogleNews.php

--- sortedGoogleNews.php
+++ sortedGoogleNews.php
@@ -75,11 +75,11 @@ margin:auto;
     $world=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiSENCQVNNQW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1EbHViVjhxQ1FvSEVnVlhiM0pzWkNnQSouCAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAVAB?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
     $business=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiTENCQVNNd29JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1EbHpNV1lxREFvS0VnaENkWE5wYm1WemN5Z0EqLggAKioICiIkQ0JBU0ZRb0lMMjB2TURWcWFHY1NCV1Z1TFVkQ0dnSkpUaWdBUAFQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
     $technology=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiT0NCQVNOUW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1EZGpNWFlxRGdvTUVncFVaV05vYm05c2IyZDVLQUEqLggAKioICiIkQ0JBU0ZRb0lMMjB2TURWcWFHY1NCV1Z1TFVkQ0dnSkpUaWdBUAFQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
-    /*$entertainment=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiU0NCQVNPQW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1ESnFhblFxRVFvUEVnMUZiblJsY25SaGFXNXRaVzUwS0FBKi4IACoqCAoiJENCQVNGUW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlnQVABUAE?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
+    $entertainment=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiU0NCQVNPQW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1ESnFhblFxRVFvUEVnMUZiblJsY25SaGFXNXRaVzUwS0FBKi4IACoqCAoiJENCQVNGUW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlnQVABUAE?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
     $sports=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiSkNCQVNNUW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1EWnVkR29xQ2dvSUVnWlRjRzl5ZEhNb0FBKi4IACoqCAoiJENCQVNGUW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlnQVABUAE?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
     $science=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiS0NCQVNNZ29JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1EWnRjVGNxQ3dvSkVnZFRZMmxsYm1ObEtBQSouCAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAVAB?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
     $health=simplexml_load_file("https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSlRpZ0FQAQ/sections/CAQiSkNCQVNNUW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlJT0NBUWFDZ29JTDIwdk1HdDBOVEVxQ2dvSUVnWklaV0ZzZEdnb0FBKi4IACoqCAoiJENCQVNGUW9JTDIwdk1EVnFhR2NTQldWdUxVZENHZ0pKVGlnQVABUAE?hl=en-IN&gl=IN&ceid=IN%3Aen") or die("Error: Cannot create object");
-   */ 
+   
 ?>
 
 <script>
@@ -90,18 +90,18 @@ obj[0] = <?php echo json_encode($india); ?>;
 obj[1] = <?php echo json_encode($world); ?>;
 obj[2] = <?php echo json_encode($business); ?>;
 obj[3] = <?php echo json_encode($technology); ?>;
-/*obj[4] = <?php echo json_encode($entertainment); ?>;
+obj[4] = <?php echo json_encode($entertainment); ?>;
 obj[5] = <?php echo json_encode($sports); ?>;
 obj[6] = <?php echo json_encode($science); ?>;
 obj[7] = <?php echo json_encode($health); ?>;
-*/
+
 var counter=-1;
 
 loadMore();
 
 function loadMore(){
     counter++;
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 8; i++) {
 
       var thread = document.createElement("div");
       thread.className="thread";
